681abaed6831e3feca6cf8463d7b4e406c33b2eb
comeintovlanc <rkdtjdwns916@gmail.com>
2023-02-23 22:50:00 +0900

    [new] 13398 solved

--- backjoon/CodePlus/basic1_1/401/13398.py
+++ backjoon/CodePlus/basic1_1/401/13398.py
@@ -1,2 +1,13 @@
 N = int(input())
 A = list(map(int,input().rstrip().split()))
+
+DP = [ [0,0] for i in range(N)]
+DP[0][0] = A[0]
+
+maxVal = A[0]
+for i in range(1,N):
+    DP[i][0] = max(DP[i-1][0]+A[i], A[i])
+    DP[i][1] = max(DP[i-1][0], DP[i-1][1] + A[i])
+    if maxVal < max(DP[i]):
+        maxVal = max(DP[i])
+print(maxVal)
