46f5c39b6cfd12d151613f99eee05b71487fea1d
comeintovlanc <rkdtjdwns916@gmail.com>
2023-03-15 23:13:30 +0900

    [new] 15656 solved

---
+++ backjoon/CodePlus/basic1_2/510/15656.py
@@ -0,0 +1,17 @@
+[N,M] = map(int,input().rstrip().split())
+A = list(map(int,input().rstrip().split()))
+
+A.sort()
+
+def printList(origin, pickedList, pick):
+    if pick == 0:
+        print(" ".join(pickedList))
+        return
+    
+    for i in range(len(origin)):
+        tmpOrigin = [_ for _ in origin]
+        tmpPickedList = [_ for _ in pickedList]
+        tmpPickedList.append(str(tmpOrigin[i]))
+        printList(tmpOrigin,tmpPickedList,pick-1)
+
+printList(A,[],M)
