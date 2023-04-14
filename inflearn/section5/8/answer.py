eb806496530fb02bc85fd0a91ff0b67aba261c10
comeintovlanc <rkdtjdwns916@gmail.com>
2023-04-14 11:43:26 +0900

    [new] inflearn/section5/8 solved

---
+++ inflearn/section5/8/answer.py
@@ -0,0 +1,26 @@
+import sys
+from collections import deque
+
+FILE_NAME = "5.txt"
+
+sys.stdin = open("out"+FILE_NAME,"rt")
+realAnswer = input()
+
+sys.stdin = open("in"+FILE_NAME,"rt")
+
+N = int(input())
+words= dict()
+
+for i in range(N):
+    words[input()] = 0
+
+for i in range(N-1):
+    words[input()] = 1
+
+for key in words:
+    if words[key] == 0:
+        print(key)
+        myAnswer = key
+        break
+
+print(myAnswer == realAnswer)
