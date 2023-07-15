441156e296c2964582c29654ccf539200af1b639
comeintovlanc <rkdtjdwns916@gmail.com>
2023-07-15 18:04:09 +0900

    [new] inflearn/section4/5 solved

---
+++ inflearn/section4/5/answer.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open("in4.txt","rt")
+
+confs = []
+
+N = int(input())
+for i in range(N):
+    confs.append(list(map(int,input().split())))
+
+confs.sort(key=lambda x:x[1])
+
+lastEd = 0
+score = 0
+for conf in confs:
+    st,ed = conf
+    if st >= lastEd:
+        score += 1
+        lastEd = ed
+print(score)
